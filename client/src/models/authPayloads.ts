bff3574773c07e19d9899b5f269df10ef4f65aac
Ordan Gramatov <ordance01@yahoo.com>
2023-04-29 20:20:04 +0200

    Created models for auth payloads

---
+++ client/src/models/authPayloads.ts
@@ -0,0 +1,12 @@
+export interface RegisterFields {
+	firstName: string,
+	lastName: string,
+	email: string,
+	password: string,
+	confirmPassword: string
+}
+
+export interface LoginFields {
+	email: string,
+	password: string,
+}
