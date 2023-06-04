2b91752eed1ec5b8ce688386d5fa0d8a3b2ba47f
Ordan Gramatov <ordance01@yahoo.com>
2023-06-04 20:41:06 +0200

    Added new endpoint

--- client/src/endpoints/session.ts
+++ client/src/endpoints/session.ts
@@ -2,11 +2,15 @@ import api from '../api';
 import { LoginFields, RegisterFields } from '../models/authPayloads';
 
 export const userRegister = function (body: RegisterFields) {
-  api.post('/auth/register', body).then((res) => res.data);
+  return api.post('/auth/register', body).then((res) => res.data);
 };
 
 export const userLogin = function (body: LoginFields) {
-  api.post('/auth/login', body).then((res) => res.data);
+  return api.post('/auth/login', body).then((res) => res.data);
+};
+
+export const currentUser = function () {
+  return api.get('/auth/current').then((res) => res.data);
 };
 
 export const userLogout = function () {
