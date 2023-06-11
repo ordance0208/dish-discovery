1ae6b1235dd3519c2392fdaab26a15c3db3a74b3
Ordan Gramatov <ordance01@yahoo.com>
2023-06-11 13:34:43 +0200

    Created profile settings view

---
+++ client/src/views/Profile/ProfileSettings.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+import useUserSettingsTabs from '../../hooks/settings/useUserSettingsTabs';
+import Tabs from '../../components/Tabs';
+import PersonalSettings from './components/PersonalSettings';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  profileView: {
+    display: 'flex',
+    maxWidth: 1440,
+    margin: 'auto',
+    height: '100vh',
+    [theme.breakpoints.down('xl')]: {
+      width: '90%',
+    },
+  },
+  tabsContainer: {
+    width: 275,
+    borderRight: '2px solid #dedede',
+  },
+  selectedTab: {
+    width: '100%',
+  },
+}));
+
+const ProfileSettings = () => {
+  const classes = useStyles();
+
+  const [tab, setTab] = useState<number>(0);
+  const { userSettingsTabs } = useUserSettingsTabs();
+
+  const renderTab = () => {
+    switch (tab) {
+      case 0:
+        return <PersonalSettings />;
+    }
+  };
+
+  return (
+    <div className={classes.profileView}>
+      <div className={classes.tabsContainer}>
+        <Tabs
+          tabs={userSettingsTabs}
+          setTabsExternal={setTab}
+          orientation='vertical'
+        />
+      </div>
+      <div className={classes.selectedTab}>{renderTab()}</div>
+    </div>
+  );
+};
+
+export default ProfileSettings;
