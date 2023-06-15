2c1578b2c0c1a5b0bed34d0ede633630fc5128f4
Ordan Gramatov <ordance01@yahoo.com>
2023-06-15 19:18:50 +0200

    Added a route for profile settings

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useAuthActions } from './utils/AuthContext/actions';
 import { PATHS, AUTH_PATHS } from './routes';
 import PublicRoute from './utils/PublicRoute';
+import PrivateRoute from './utils/PrivateRoute';
 import Home from './views/Home';
 import SignUp from './views/Auth/SignUp';
 import SignIn from './views/Auth/SignIn';
@@ -27,7 +28,10 @@ const Router = () => {
         <Route element={<SignUp />} path={AUTH_PATHS.REGISTER} />
         <Route element={<SignIn />} path={AUTH_PATHS.LOGIN} />
       </Route>
-      <Route path={PATHS.PROFILE_SETTINGS} element={<ProfileSettings />}/>
+      <Route element={<PrivateRoute />}>
+        <Route element={<ProfileSettings />} path={PATHS.PROFILE_SETTINGS} />
+      </Route>
+      <Route path={PATHS.PROFILE_SETTINGS} element={<ProfileSettings />} />
     </Routes>
   );
 };
