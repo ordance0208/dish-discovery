7b66db8e17fa5a53e7e19b6e473e0489101940d9
Ordan Gramatov <ordance01@yahoo.com>
2023-06-15 19:17:28 +0200

    Modified endpoint

--- server/src/routers/authRouter.ts
+++ server/src/routers/authRouter.ts
@@ -13,7 +13,7 @@ const authRouter = express.Router();
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', authenticateToken as any, logoutUser as any);
-authRouter.post('/logoutall', authenticateToken as any, logoutAll as any);
+authRouter.post('/logout-all', authenticateToken as any, logoutAll as any);
 authRouter.get('/current', authenticateToken as Application, getCurrentUser as any)
 
 export default authRouter;
