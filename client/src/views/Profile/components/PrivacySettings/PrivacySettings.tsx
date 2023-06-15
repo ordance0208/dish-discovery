a750999b3a20de4dc2dc374cc566df5449c67f79
Ordan Gramatov <ordance01@yahoo.com>
2023-06-15 19:20:47 +0200

    Created privacy settings tab

---
+++ client/src/views/Profile/components/PrivacySettings/PrivacySettings.tsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { TEXT_DARK } from '../../../../theme';
+import useUserPrivacySettings from '../../../../hooks/settings/useUserPrivacySettings';
+import Typography from '../../../../components/Typography';
+import PasswordField from '../../../../components/PasswordField/PasswordField';
+import Button from '../../../../components/Button';
+import Dialog from '../../../../components/Dialog';
+
+const useStyles = makeStyles({
+  privacySettingsContent: {
+    width: 500,
+    margin: '50px auto',
+  },
+  subtitle: {
+    fontWeight: 600,
+    fontSize: 16,
+    color: TEXT_DARK,
+  },
+  passwordFieldContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 24,
+    marginTop: 24,
+  },
+  section: {
+    marginBottom: 50,
+  },
+  red: {
+    fontSize: 12,
+    color: 'red',
+    marginBottom: 12,
+  },
+  dialogText: {
+    color: TEXT_DARK,
+  },
+  changeButton: {
+    alignSelf: 'flex-start',
+  },
+});
+
+const PrivacySettings = () => {
+  const classes = useStyles();
+
+  const { handleLogoutAllSessions, handleDeleteAccount } =
+    useUserPrivacySettings();
+
+  const [action, setAction] = useState<'logout' | 'delete' | null>(null);
+
+  return (
+    <div className={classes.privacySettingsContent}>
+      <div className={classes.section}>
+        <Typography className={classes.subtitle}>
+          Change your password
+        </Typography>
+        <div className={classes.passwordFieldContainer}>
+          <PasswordField label='Current password' />
+          <PasswordField label='New password' />
+          <PasswordField label='Confirm new password' />
+          <Button
+            className={classes.changeButton}
+            onClick={() => setAction('logout')}
+          >
+            Change password
+          </Button>
+        </div>
+      </div>
+      <div className={classes.section}>
+        <Typography className={classes.subtitle}>
+          Log out of all sessions
+        </Typography>
+        <Typography className={classes.red}>
+          This action will log you out from your current session as well.
+        </Typography>
+        <Button color='warning' onClick={() => setAction('logout')}>
+          Log out
+        </Button>
+      </div>
+      <div className={classes.section}>
+        <Typography className={classes.subtitle}>Delete account</Typography>
+        <Typography className={classes.red}>
+          This action will permamently delete your account and you will not be
+          able to log in again.
+        </Typography>
+        <Button color='warning' onClick={() => setAction('delete')}>
+          Delete account
+        </Button>
+      </div>
+      <Dialog
+        open={!!action}
+        onClose={() => setAction(null)}
+        onConfirm={
+          action === 'delete' ? handleDeleteAccount : handleLogoutAllSessions
+        }
+        title={
+          action === 'delete'
+            ? 'Delete Your account'
+            : 'Log out of all sessions'
+        }
+        confirmButtonColor='warning'
+      >
+        <Typography className={classes.dialogText}>
+          {action === 'delete'
+            ? 'Are you sure you want to delete your account?'
+            : 'Are you sure you want to log out of all sessions?'}
+        </Typography>
+      </Dialog>
+    </div>
+  );
+};
+
+export default PrivacySettings;
