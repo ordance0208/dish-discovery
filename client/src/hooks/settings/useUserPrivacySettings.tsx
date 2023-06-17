ab9625def40c03a141159ac66fe5264b006bb4ef
Ordan Gramatov <ordance01@yahoo.com>
2023-06-17 18:34:41 +0200

    updated privacy settings hook

--- client/src/hooks/settings/useUserPrivacySettings.tsx
+++ client/src/hooks/settings/useUserPrivacySettings.tsx
@@ -1,8 +1,31 @@
-import { useAuthActions } from '../../utils/AuthContext/actions';
+import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import { useAuthActions } from '../../utils/AuthContext/actions';
 import { AUTH_PATHS } from '../../routes';
 
 const useUserPrivacySettings = () => {
+  const initialValues = {
+    currentPassword: '',
+    newPassword: '',
+    confirmNewPassword: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    currentPassword: Yup.string().required(
+      'Please enter your current password'
+    ),
+    newPassword: Yup.string()
+      .matches(
+        // eslint-disable-next-line
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        'The password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and a special character'
+      )
+      .required('Please enter a new password'),
+    confirmNewPassword: Yup.string()
+      .required('Please confirm your new password')
+      .oneOf([Yup.ref('password')], "Passwords don't match"),
+  });
+
   const navigate = useNavigate();
   const { logoutAllSessions } = useAuthActions();
 
@@ -14,8 +37,10 @@ const useUserPrivacySettings = () => {
   const handleDeleteAccount = async () => {};
 
   return {
+    initialValues,
+    validationSchema,
     handleLogoutAllSessions,
-		handleDeleteAccount
+    handleDeleteAccount,
   };
 };
 
