36af949712d5f7d5243a761ca5633a4930a3d42d
Ordan Gramatov <ordance01@yahoo.com>
2023-07-19 17:38:13 +0200

    Added static middleware for avatars and new route

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,8 @@ env.config();
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routers/authRouter';
-const SERVER_PORT = process.env.SERVER_PORT || 8000
+import userRouter from './routers/userRouter';
+const SERVER_PORT = process.env.SERVER_PORT || 8000;
 import connectToDatabase from './db/mongoose';
 
 const app = express();
@@ -11,13 +12,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send(200)
-})
+app.use('/avatar', express.static('avatar'));
 
 // Routers
-app.use('/api/auth' , authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
 
 app.listen(SERVER_PORT, () => {
   console.log('Server is running on port: ' + SERVER_PORT);
