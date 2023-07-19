ca939d0cacdc8182b4cfcc9d9d7770481748fee7
Ordan Gramatov <ordance01@yahoo.com>
2023-07-19 18:23:00 +0200

    Added error showing on sign up

--- client/src/views/Auth/SignUp/SignUp.tsx
+++ client/src/views/Auth/SignUp/SignUp.tsx
@@ -1,12 +1,15 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 import useSignupForm from '../../../hooks/auth/useSignupForm';
 import TextField from '../../../components/TextField';
 import PasswordField from '../../../components/PasswordField';
 import Button from '../../../components/Button';
 import Typography from '../../../components/Typography';
 import AuthView from '../../../components/AuthView';
+import { IResponse } from '../../../models/response';
+import Alert from '../../../components/Alert';
 
 const useStyles = makeStyles((theme: Theme) => ({
   signupForm: {
@@ -36,12 +39,26 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontWeight: 600,
     color: theme.palette.primary.main,
   },
+  alert: {
+    marginTop: 16,
+  },
 }));
 
 const SignUp = () => {
   const classes = useStyles();
 
-  const { initialValues, validationSchema, handleSignup } = useSignupForm();
+  
+  const [response, setResponse] = useState<IResponse | undefined>();
+
+  useEffect(() => {
+    if (!response) return;
+
+    setTimeout(() => {
+      setResponse(undefined);
+    }, 5000);
+  }, [response]);
+  
+  const { initialValues, validationSchema, handleSignup } = useSignupForm(setResponse);
 
   return (
     <AuthView
@@ -51,38 +68,50 @@ const SignUp = () => {
       isLogin={false}
     >
       {(props: any) => (
-        <form onSubmit={props.handleSubmit} className={classes.signupForm}>
-          <div className={classes.fullName}>
+        <>
+          <form onSubmit={props.handleSubmit} className={classes.signupForm}>
+            <div className={classes.fullName}>
+              <TextField
+                className={classes.name}
+                label='First name'
+                name='firstName'
+                required
+              />
+              <TextField
+                className={classes.name}
+                label='Last name'
+                name='lastName'
+                required
+              />
+            </div>
             <TextField
-              className={classes.name}
-              label='First name'
-              name='firstName'
+              label='Email address'
+              name='email'
+              type='email'
               required
             />
-            <TextField
-              className={classes.name}
-              label='Last name'
-              name='lastName'
-              required
-            />
-          </div>
-          <TextField label='Email address' name='email' type='email' required />
-          <PasswordField name='password' label='Password' />
-          <PasswordField name='confirmPassword' label='Confirm password' />
-          <Button
-            disabled={props.isSubmitting}
-            onClick={props.handleSubmit}
-            type='submit'
-          >
-            Sign Up
-          </Button>
-          <Typography className={classes.haveAccount}>
-            Already have an account?{' '}
-            <Link to='/login' className={classes.signinLink}>
-              Log in
-            </Link>
-          </Typography>
-        </form>
+            <PasswordField name='password' label='Password' />
+            <PasswordField name='confirmPassword' label='Confirm password' />
+            <Button
+              disabled={props.isSubmitting}
+              onClick={props.handleSubmit}
+              type='submit'
+            >
+              Sign Up
+            </Button>
+            <Typography className={classes.haveAccount}>
+              Already have an account?{' '}
+              <Link to='/login' className={classes.signinLink}>
+                Log in
+              </Link>
+            </Typography>
+          </form>
+          {response && (
+            <Alert className={classes.alert} severity={response.severity}>
+              {response.text}
+            </Alert>
+          )}
+        </>
       )}
     </AuthView>
   );
