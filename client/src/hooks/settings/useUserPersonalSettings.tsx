db4713915e6888e4812a696805971914bc944abc
Ordan Gramatov <ordance01@yahoo.com>
2023-07-19 15:00:24 +0200

    Added handlers for updating perosnal information

--- client/src/hooks/settings/useUserPersonalSettings.tsx
+++ client/src/hooks/settings/useUserPersonalSettings.tsx
@@ -1,9 +1,19 @@
 import * as Yup from 'yup';
+import { useAuthActions } from '../../utils/AuthContext/actions';
 import { useAuthData } from '../../utils/AuthContext/selectors';
 import { avatarOptions } from '../../utils/settings.helpers';
+import { PersonalInfoPayload } from '../../models/user/userSettingsPayloads';
+import { IResponse } from '../../models/response';
 
-const useUserPersonalSettings = () => {
+const useUserPersonalSettings = (
+  setResponse: React.Dispatch<React.SetStateAction<IResponse | undefined>>
+) => {
   const { user } = useAuthData();
+
+  const { updateUserInfo, uploadUserAvatar, removeUserAvatar } =
+    useAuthActions();
+
+  const avatarUrl = user?.avatar;
 
   const initialValues = {
     firstName: user?.firstName || '',
@@ -19,15 +29,52 @@ const useUserPersonalSettings = () => {
       .required('Please enter an email address'),
   });
 
-  const onProfileUpdate = (values: any) => {
-    console.log(values);
+  const handleErrorResponse = (text: string) => {
+    setResponse({ severity: 'warning', text });
+  };
+
+  const handleSuccessResponse = (text: string) => {
+    setResponse({ severity: 'success', text });
+  };
+
+  const onProfileUpdate = async (values: PersonalInfoPayload) => {
+    try {
+      await updateUserInfo(values);
+      handleSuccessResponse('Profile info updated successfully!');
+    } catch (err: any) {
+      handleErrorResponse(err.message);
+    }
+  };
+
+  const handleAvatarUpload = async (file: any) => {
+    const formData = new FormData();
+    formData.append('avatar', file);
+    try {
+      await uploadUserAvatar(formData);
+      handleSuccessResponse('Avatar uploaded successfully!');
+    } catch (err: any) {
+      handleErrorResponse(err.message);
+    }
+  };
+
+  const handleRemoveAvatar = async () => {
+    try {
+      await removeUserAvatar();
+      handleSuccessResponse('Avatar removed successfully!');
+    } catch (err: any) {
+      handleErrorResponse(err.message);
+    }
   };
 
   return {
     avatarOptions,
     initialValues,
     validationSchema,
+    avatarUrl,
+    user,
     onProfileUpdate,
+    handleAvatarUpload,
+    handleRemoveAvatar,
   };
 };
 
