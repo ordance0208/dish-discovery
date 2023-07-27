5098846c29f16d8f730bfa35cf1cbb1e6d538820
Ordan Gramatov <ordance01@yahoo.com>
2023-07-27 17:16:31 +0200

    Refactored droppable component

--- client/src/components/Droppable/Droppable.tsx
+++ client/src/components/Droppable/Droppable.tsx
@@ -1,18 +1,26 @@
 import React, { ChangeEvent, DragEvent, useState, useRef } from 'react';
-import { Theme } from '@mui/material';
+import clsx from 'clsx';
 import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material';
 import { UilTimes } from '@iconscout/react-unicons';
 import Typography from '../Typography';
 import IconButton from '../IconButton';
 
 interface Props {
   text?: string;
+  fileError?: boolean;
+  customError?: string;
+  setFile?: React.Dispatch<React.SetStateAction<File | null>>;
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
+  droppableWrapper: {
+    display: 'flex',
+    gap: 32,
+  },
   droppable: {
     borderRadius: 4,
-    border: '2px dashed lightgray',
+    border: '2px dashed rgb(188, 188, 188)',
     width: 500,
     height: 300,
     display: 'flex',
@@ -25,21 +33,47 @@ const useStyles = makeStyles((theme: Theme) => ({
   fileName: {
     display: 'flex',
     gap: 16,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
+    maxWidth: '100%',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    textAlign: 'center',
+  },
+  uploadedImage: {
+    height: 150,
+    width: 150,
+    objectFit: 'cover',
+    alignSelf: 'flex-end',
   },
   input: {
     display: 'none',
   },
+  error: {
+    border: `2px dashed ${theme.palette.error.main}`,
+  },
+  errorText: {
+    color: theme.palette.error.main,
+  },
 }));
 
-const Droppable = ({ text }: Props) => {
+const Droppable = ({
+  text,
+  fileError,
+  customError,
+  setFile: setFileHoist,
+}: Props) => {
   const classes = useStyles();
   const [file, setFile] = useState<File | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleUploadClick = () => {
+    if (file) return;
     inputRef?.current?.click();
   };
 
@@ -47,11 +81,12 @@ const Droppable = ({ text }: Props) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
-    setFile(file);
+    handleImageUpload(file);
   };
 
-  const handleImageUpload = (file: File) => {
+  const handleImageUpload = (file: File | null) => {
     setFile(file);
+    setFileHoist?.(file!);
   };
 
   const handleOnDrop = (e: DragEvent<HTMLDivElement>) => {
@@ -71,34 +106,51 @@ const Droppable = ({ text }: Props) => {
 
   const handleRemoveFile = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
-    setFile(null);
+    handleImageUpload(null);
+    inputRef!.current!.value = '';
+  };
+
+  const getImageUrl = () => {
+    if (!file) return undefined;
+    const url = URL.createObjectURL(file);
+    return url;
   };
 
   return (
     <>
-      <div
-        className={classes.droppable}
-        onClick={handleUploadClick}
-        onDrop={handleOnDrop}
-        onDragOver={handleDragOver}
-      >
-        <div className={classes.fileName}>
-          <Typography className={classes.text}>
-            {file?.name || text || 'Drag and drop or click to upload an image'}
-          </Typography>
-          {!!file && (
-            <IconButton onClick={handleRemoveFile}>
-              <UilTimes size={18} />
-            </IconButton>
-          )}
+      <div className={classes.droppableWrapper}>
+        <div
+          className={clsx(classes.droppable, { [classes.error]: fileError })}
+          onClick={handleUploadClick}
+          onDrop={handleOnDrop}
+          onDragOver={handleDragOver}
+        >
+          <div className={classes.fileName}>
+            <Typography
+              className={clsx(classes.text, { [classes.errorText]: fileError })}
+            >
+              {(fileError && (customError || 'Upload file')) ||
+                file?.name ||
+                text ||
+                'Drag and drop or click to upload an image'}
+            </Typography>
+            {!!file && (
+              <IconButton onClick={handleRemoveFile}>
+                <UilTimes size={18} />
+              </IconButton>
+            )}
+          </div>
+          <input
+            ref={inputRef}
+            className={classes.input}
+            type='file'
+            accept='image/*'
+            onChange={handleInputChange}
+          />
         </div>
-        <input
-          ref={inputRef}
-          className={classes.input}
-          type='file'
-          accept='image/*'
-          onChange={handleInputChange}
-        />
+        {!!file && (
+          <img className={classes.uploadedImage} src={getImageUrl()} alt='submitted'/>
+        )}
       </div>
     </>
   );
