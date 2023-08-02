e8f9df3df4a505f89978dc9862e7735b2bbbac94
Ordan Gramatov <ordance01@yahoo.com>
2023-08-02 15:56:53 +0200

    Modified recipe endpoints

--- client/src/endpoints/recipe.ts
+++ client/src/endpoints/recipe.ts
@@ -4,8 +4,18 @@ export const createRecipe = function (formData: FormData) {
   return api.post('/recipes', formData).then((res) => res.data);
 };
 
-export const getRecipe = function (id: string) {
+export const getAllRecipes = function () {
+  return api.get(`/recipes`).then((res) => res.data);
+};
+
+export const getSingleRecipe = function (id: string) {
   return api.get(`/recipes/${id}`).then((res) => res.data);
+};
+
+export const likeRecipe = function (id: string, likeStatus: boolean) {
+  return api
+    .put(`/recipes/${id}/like`, undefined, { params: { status: likeStatus } })
+    .then((res) => res.data);
 };
 
 export const editRecipe = function (id: string, formData: FormData) {
