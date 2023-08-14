8ec49927085ba7e635246c428dadc80d88465c7a
Ordan Gramatov <ordance01@yahoo.com>
2023-08-14 16:13:56 +0200

    Added submit function

--- client/src/hooks/recipe/useSubmitRecipe.tsx
+++ client/src/hooks/recipe/useSubmitRecipe.tsx
@@ -1,9 +1,17 @@
 import * as Yup from 'yup';
+import { Descendant } from 'slate';
+import { createRecipe } from '../../endpoints/recipe';
+import { RecipeFields } from '../../models/recipe/recipePayload';
+import { IResponse } from '../../models/response';
 
-const useSubmitRecipe = () => {
+const useSubmitRecipe = (
+  setRecipeSubmitted: React.Dispatch<React.SetStateAction<string | null>>
+) => {
   const initialValues = {
     title: '',
-    description: [{ type: 'paragraph', children: [{ text: '' }] }],
+    description: [
+      { type: 'paragraph', children: [{ text: '' }] },
+    ] as unknown as Descendant[],
     ingredients: [''],
     preparationTime: 0,
     tags: [],
@@ -28,8 +36,48 @@ const useSubmitRecipe = () => {
       .required(),
   });
 
-  const onSubmit = (values: any) => {
-    console.log(values);
+  const onSubmit = async (
+    values: RecipeFields,
+    recipeImage: File,
+    setSubmitting: (isSubmitting: boolean) => void,
+    setResponse: React.Dispatch<React.SetStateAction<IResponse | undefined>>
+  ) => {
+    setSubmitting(true);
+    const formData = new FormData();
+
+    formData.append('title', values.title);
+    formData.append('recipe', recipeImage);
+    formData.append(
+      'preparationTime',
+      values.preparationTime as unknown as string
+    );
+
+    values.tags.forEach((tag: string, index: number) => {
+      formData.append(`tags[${index}]`, tag);
+    });
+
+    values.ingredients.forEach((ingredient: string, index: number) => {
+      formData.append(`ingredients[${index}]`, ingredient);
+    });
+
+    values.description.forEach((row: any, index: number) => {
+      formData.append(`description[${index}][type]`, row.type);
+      row.children.forEach((children: any, i: number) => {
+        formData.append(
+          `description[${index}][children][${i}][text]`,
+          children.text as string
+        );
+      });
+    });
+
+    try {
+      const data = await createRecipe(formData);
+      setRecipeSubmitted(data._id);
+    } catch (err: any) {
+      setResponse({ text: err.response.data.error, severity: 'warning' });
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return { initialValues, validationSchema, onSubmit };
