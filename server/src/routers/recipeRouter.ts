28c5e770d9c4e246f9b3c3af071c06abde836cc9
Ordan Gramatov <ordance01@yahoo.com>
2023-08-17 15:35:22 +0200

    Added additional recipe routes

--- server/src/routers/recipeRouter.ts
+++ server/src/routers/recipeRouter.ts
@@ -1,23 +1,47 @@
 import express, { Application } from 'express';
+import multer from 'multer';
 import {
   recipe,
+  storage,
   createRecipe,
   getRecipe,
+  getRecipeForEdit,
   editRecipe,
   deleteRecipe,
+  likeRecipe,
+  getRecipes,
 } from '../controllers/recipeController';
 import authenticateToken from '../middleware/auth';
 
 const recipeRouter = express.Router();
 
+const upload = multer({ storage });
+
 recipeRouter.post(
   '/',
   authenticateToken as Application,
-  recipe.single('recipe'),
+  upload.single('recipe'),
   createRecipe as any
 );
+recipeRouter.get('/', getRecipes as any);
 recipeRouter.get('/:id', getRecipe as any);
-recipeRouter.put('/:id', authenticateToken as Application, recipe.single('recipe'), editRecipe as any);
+recipeRouter.get(
+  '/edit/:id',
+  authenticateToken as Application,
+  getRecipeForEdit as any
+);
+recipeRouter.put(
+  '/:id',
+  authenticateToken as Application,
+  recipe.single('recipe'),
+  editRecipe as any
+);
+recipeRouter.put(
+  '/:id/like',
+  authenticateToken as Application,
+  recipe.single('recipe'),
+  likeRecipe as any
+);
 recipeRouter.delete(
   '/:id',
   authenticateToken as Application,
