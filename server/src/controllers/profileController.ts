6d8d70ce9a434ec2b5cf94d694af3b6232731a4d
Ordan Gramatov <ordance01@yahoo.com>
2023-08-20 16:15:55 +0200

    Added endpoints for fetching user profiles

---
+++ server/src/controllers/profileController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import Recipe from '../models/Recipe';
+import User from '../models/User';
+
+export const getUserProfile = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    const userRecipes = await Recipe.find({ user: user._id }, null, {
+      sort: { createdAt: -1 },
+    }).populate({ path: 'user' });
+
+    res.send({ user, userRecipes });
+  } catch (err: any) {
+    return res.status(500).send(err);
+  }
+};
