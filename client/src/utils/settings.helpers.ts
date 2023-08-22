18ef8699f77fc85e8b6f13eabd55056e6c6d81c7
Ordan Gramatov <ordance01@yahoo.com>
2023-08-22 13:56:21 +0200

    Commented preview profile option

--- client/src/utils/settings.helpers.ts
+++ client/src/utils/settings.helpers.ts
@@ -5,10 +5,10 @@ export const avatarOptions = [
     label: 'Add profile picture',
     value: AvatarActions.ADD_PROFILE_PICTURE,
   },
-  {
-    label: 'Preview profile picture',
-    value: AvatarActions.PREVIEW_PROFILE_PICTURE,
-  },
+  // {
+  //   label: 'Preview profile picture',
+  //   value: AvatarActions.PREVIEW_PROFILE_PICTURE,
+  // },
   {
     label: 'Remove profile picture',
     value: AvatarActions.DELETE_PROFILE_PICTURE,
